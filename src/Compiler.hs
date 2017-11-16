5f600da3abbf6a8b5541e7423568936561953114
fsawady <sawady.faso@gmail.com>
2017-11-16 16:55:35 -0300

    compilation of classes

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -3,9 +3,27 @@ module Compiler where
 import AST
 import Generator
 import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 
-printPrelude :: String
-printPrelude = unlines [
+type MethodInfo = String
+type ClassInfo = (String, Int, [MethodInfo])
+type ClassesInfo = M.Map String ClassInfo
+type SelectorsTable = M.Map String String
+
+primitiveClasses :: [ClassInfo]
+--                  cls0                               cls1
+primitiveClasses = [("Int", 1, ["print/0", "add/1"]), ("String", 1, ["print/0", "add/1"])]
+
+primitiveSelectors :: [String]
+--                    sel0       sel1
+primitiveSelectors = ["print/0", "add/1"]
+
+compileWith :: ClassesInfo -> String
+compileWith classesInfo = unlines $ 
+    compilePrelude ++ compileClasses classesInfo ++ compileMethods classesInfo
+
+compilePrelude :: [String]
+compilePrelude = [
         "typedef unsigned long long int Num;",
         "typedef char* String;",
         "typedef void* PTR;",
@@ -30,35 +48,83 @@ printPrelude = unlines [
         "#define OBJECT_TO_PTR(O) ((PTR)(O)) /* Convierte Objeto * -> PTR */",
         "#define PTR_TO_OBJECT(P) ((Objeto*)(P)) /* Convierte PTR -> Objeto * */",
         ""
+    ] ++ compilePrimitivesClasses
+
+compilePrimitivesClasses :: [String]
+compilePrimitivesClasses = [
+        "/* Construye un objeto de clase Int */",
+        "Objeto* constructor_cls0(Num valor) {",
+        "  Objeto* obj = new Objeto;",
+        "  obj->clase = cls0; /* Int */",
+        "  obj->varsInstancia = new PTR[1];",
+        "  obj->varsInstancia[0] = NUM_TO_PTR(valor);",
+        "  return obj;",
+        "}",
+        "",
+        "/* Construye un objeto de clase String */",
+        "Objeto* constructor_cls1(String valor) {",
+        "  Objeto* obj = new Objeto;",
+        "  obj->clase = cls1; /* String */",
+        "  obj->varsInstancia = new PTR[1];",
+        "  obj->varsInstancia[0] = STRING_TO_PTR(valor);",
+        "  return obj;",
+        "}",
+        ""
     ]
 
-collectClasses :: Program -> [String]
-collectClasses classes = map (\(Class n _ _) -> n) classes
+compileClasses :: ClassesInfo -> [String]
+compileClasses m = compileClasses' $ filter (\(c,_) -> not $ elem c primitives) (M.assocs m)
+    where compileClasses' = foldr (\(c1, (c2, n, _)) r -> compileClass c1 c2 n ++ r) []
+          primitives = map (\(c, _, _) -> c) primitiveClasses
 
-collectSelectors :: Program -> [String]
-collectSelectors classes = concat $ map collectOnClasses classes
-    where collectOnClasses (Class _ _ methods) = map collectOnMethods methods
-          collectOnMethods (Method n params block) = (n ++ "/" ++ show (length params))
+compileClass :: String -> String -> Int -> [String]
+compileClass origCls cls n = [
+        "/* Construye un objeto de la clase " ++ origCls ++ " */",
+        "Objeto* constructor_" ++ cls ++ "() {",
+        "  Objeto* obj = new Objeto;",
+        "  obj->clase = " ++ cls ++ "; /* " ++ origCls ++ " */"
+    ] ++ compileLocals n ++ ["  return obj", "}", ""] 
 
+compileLocals :: Int -> [String]
+compileLocals n = 
+    ("  obj->varsInstancia = new PTR[" ++ show n ++ "];") : 
+        map (\i -> "  obj->varsInstancia[" ++ show i ++ "] = constructor_cls0(0);") [0..n-1]
 
-classesTable :: Program -> M.Map String String
+compileMethods :: ClassesInfo -> [String]
+compileMethods m = undefined
+
+collectClasses :: Program -> [ClassInfo]
+collectClasses classes = map (\(Class n locals methods) -> (n, length locals, collectMethods methods)) classes
+
+classesTable :: Program -> ClassesInfo
 classesTable classes = 
-    foldr (\(s, i) m -> M.insert s ("cls" ++ show i) m) 
+    foldr (\((s, nlocals, methods), i) m -> M.insert s ("cls" ++ show i, nlocals, methods) m)
         M.empty 
-        (zip (collectClasses classes) [0..])
-                    
-selectorsTable :: Program -> M.Map String String
+        (zip (primitiveClasses ++ (collectClasses classes)) [0..])
+
+collectSelectorsSet :: Program -> S.Set String
+collectSelectorsSet classes = foldr (\x s -> S.insert x s) S.empty $ concat $ map collectOnClasses classes
+    where collectOnClasses (Class _ _ methods) = collectMethods methods
+
+collectMethods :: [Method] -> [String]
+collectMethods methods = map collectMethod methods
+
+collectMethod :: Method -> String
+collectMethod (Method n params block) = n ++ "/" ++ show (length params)              
+
+selectorsTable :: Program -> SelectorsTable
 selectorsTable classes = 
     foldr (\(s, i) m -> M.insert s ("sel" ++ show i) m)
           M.empty
-          (zip (collectSelectors classes) [0..])
+          (zip (primitiveSelectors ++ S.elems (collectSelectorsSet classes)) [0..])
 
 compile :: IO ()
 compile = do
     grammar <- readFile "remolacha.ll"
     input   <- readFile "example.rm"
     let program = toProgram $ parseTermino grammar input
-    -- mapM_ print $ collectClasses program
-    -- mapM_ print $ collectSelectors program
+    let sTable = selectorsTable program 
+    mapM_ print $ M.assocs sTable
     mapM_ print $ M.assocs (classesTable program)
-    mapM_ print $ M.assocs (selectorsTable program)
+    putStrLn ""
+    putStrLn $ compileWith (classesTable program)
