8d1d13e9d5e480f71e905af8332b4aa96aba2b52
Damian Lattenero <damianlattenero@gmail.com>
2017-11-22 16:19:26 -0300

    Cada vez que se accede o se asigna una variable, el nombre de dicha variable debe ser el nombre de una variable de instancia o de un parámetro que se encuentren en scope

--- src/Validations.hs
+++ src/Validations.hs
@@ -10,6 +10,7 @@ checkForError classes
     | (any hasDupl $ getMethods classes)                                        = error "duplicate method"
     | any id (map (any hasDupl) $ getMethodsParams classes)                     = error "duplicate method param name"
     | any id (compareOnebyOne (getLocals classes) (getMethodsParams classes))   = error "instance variable and param name cannot be equals"
+    | not $ allClassesInScope classes                                           = error "variable not initialized"
     | otherwise                                                                 = "All Validations pass"
 
 
@@ -22,6 +23,10 @@ compareOnebyOne :: [[String]] -> [[[String]]] -> [Bool]
 compareOnebyOne [] _          = []
 compareOnebyOne (x:xs) (y:ys) = (any (compareList x) y) : compareOnebyOne xs ys
 
+allClassesInScope :: [Class] -> Bool
+allClassesInScope []          = True
+allClassesInScope ((Class _ locals methods) : xs) = (allInScope locals methods) && (allClassesInScope xs)
+
 getClass         = map (\(Class name locals methods) -> name)
 getLocals        = map (\(Class name locals methods) -> locals)
 getMethods       = map (\(Class name locals methods) -> map getName methods)
@@ -30,9 +35,20 @@ getName (Method name params _) = (name, length params)
 getMethodParams (Method _ params _) = params 
 compareList ls = not . null . intersect ls
 
+allInScope locals methods = all (isInScope locals) methods
+isInScope locals (Method _ variables exps) = compareList (locals ++ variables) (concat $ map collectAssigns exps)
+
+collectAssigns (Assign var exp)   = var : collectAssigns exp
+collectAssigns (Send _ exprs exp) = collectAssigns exp ++ concat (map collectAssigns exprs)
+collectAssigns _                  = []
+
+--[varName | x@(Assign varName _) <- exps]
+
+
+
 testValidations :: IO ()
 testValidations = do
     grammar <- readFile "remolacha.ll"
-    input   <- readFile "test1.rm"
+    input   <- readFile "test2.rm"
     let program = toProgram $ parseTermino grammar input
     putStrLn (checkForError program)
