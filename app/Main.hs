fef2ca0d5ec9b3f1bcbe5f983a945bc248046b12
fsawady <sawady.faso@gmail.com>
2017-11-22 16:56:29 -0300

    main program

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,45 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
+import Control.Monad(when)
+import System.Console.CmdArgs
+import System.Directory
+import System.Environment
+import System.Exit
+import System.IO
+import System.Process
+
 import Compiler
-import Validations
+
+data RemolachaArgs = RemolachaArgs {
+    file :: String,
+    output :: String
+ } deriving (Show, Data, Typeable)
+
+llecaConsole = RemolachaArgs {
+    file        = def &= help "Input file" &= typ "[input]" &= name "f",
+    output      = def &= help "Output file" &= typ "[input]" &= name "o"
+ }
 
 main :: IO ()
--- main = compile
-main = testValidations
+main = do
+    args <- (cmdArgs $ llecaConsole
+        &= help "The Remolacha compiler"
+        &= program "Remolacha"
+        &= summary "Remolacha v1.0.0.0"
+     )
+
+    dir <- getCurrentDirectory
+
+    inputFile <- (canonicalizePath (file args))
+    existsGrammar <- doesFileExist (inputFile)
+    when (not existsGrammar) (die ("The given input file " ++ (file args) ++ " does not exist"))
+
+    outputFile <- (canonicalizePath (output args))
+
+    fileContents <- readFile inputFile
+
+    putStrLn ""
+
+    -- compile
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,4 @@
+import Validations
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = testValidations
