c0e366bc286696d32cb36775acba7c382379db92
Damian Lattenero <damianlattenero@gmail.com>
2017-11-23 13:39:20 -0300

    fix tabs in tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,15 +9,16 @@ main = test
 
 test :: IO ()
 test = hspec $ do
-	describe "validations" $ do
-		it "duplicate class" $ do
-			grammar <- readFile "remolacha.ll"
-			input   <- readFile "test/input/duplicate_class.rm"
-			let program = toProgram $ parseTermino grammar input
-			checkForError program `shouldBe` (Left "duplicate class")
+    describe "validations" $ do
+        it "duplicate class" $ do
+            grammar <- readFile "remolacha.ll"
+            input   <- readFile "test/input/duplicate_class.rm"
+            let program = toProgram $ parseTermino grammar input
+            checkForError program `shouldBe` (Left "duplicate class")
 
-		it "duplicate variable name" $ do
-			grammar <- readFile "remolacha.ll"
-			input   <- readFile "test/input/duplicate_variable_name.rm"
-			let program = toProgram $ parseTermino grammar input
-			checkForError program `shouldBe` (Left "duplicate variable name")
+
+        it "duplicate variable name" $ do
+            grammar <- readFile "remolacha.ll"
+            input   <- readFile "test/input/duplicate_variable_name.rm"
+            let program = toProgram $ parseTermino grammar input
+            checkForError program `shouldBe` (Left "duplicate variable name")
